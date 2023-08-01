e1bae291475bd4c02c81e57c794c3176c8944b32
kylenissen <nissenkyle@gmail.com>
2023-07-31 19:59:58 -0600

    Adding header to all files for GNU LGPL license Removed duplicate dll's for NUnit Adding .editorConfig to GameCore project to add headers faster

--- GameCore/GameCore/Testing/TestRunner.cs
+++ GameCore/GameCore/Testing/TestRunner.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// Copyright 2023 Vivid Helix
+// This file is part of ViviHelixTestFramework.
+// ViviHelixTestFramework is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// ViviHelixTestFramework is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License along with ViviHelixTestFramework. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO;
@@ -78,7 +86,7 @@ namespace VividHelix.HotReload.Tests
             if (Input.GetKeyDown(runTestsKeyCode))
             {
                 if(infoDisplay != null)
-                    infoDisplay.text = "Running Tests";
+                    infoDisplay.text = "Running Tests. Check root folder for XML output.";
 
                 RunTestsInThread();
             }
