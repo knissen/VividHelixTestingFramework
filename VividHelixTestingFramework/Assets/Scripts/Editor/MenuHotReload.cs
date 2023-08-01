9f7702f43eb6d140eb5d6b3335efee7f2a97f6a4
kylenissen <nissenkyle@gmail.com>
2023-07-31 20:39:51 -0600

    Adding GNU LGPL to scripts in Unity project

--- VividHelixTestingFramework/Assets/Scripts/Editor/MenuHotReload.cs
+++ VividHelixTestingFramework/Assets/Scripts/Editor/MenuHotReload.cs
@@ -1,3 +1,11 @@
+// Copyright 2023 Vivid Helix
+// This file is part of ViviHelixTestFramework.
+// ViviHelixTestFramework is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// ViviHelixTestFramework is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License along with ViviHelixTestFramework. If not, see <https://www.gnu.org/licenses/>.
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
